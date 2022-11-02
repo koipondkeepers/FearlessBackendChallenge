d87bc3f696b4976a1e77548d56b6b42ef177dc0d
victor lin <v.lin01@gmail.com>
2022-11-01 21:34:05 -0400

    create post method to add items and save them inside data directory

--- index.js
+++ index.js
@@ -6,5 +6,29 @@ const app = express();
 
 app.use(express.json());
 
-
 app.listen(3000, () => console.log("Server is now running"));
+
+app.get("/items", (req, res) => {
+  
+})
+
+app.post("/items/addItem", (req,res) => {
+  const id = uuid();
+  const {itemName} = req.body;
+  if (!itemName) {
+    return res.sendStatus(400);
+  }
+
+  const item = {
+    id: id,
+    item: itemName
+  }
+
+  fs.mkdirSync("data/items", {recursive: true});
+  fs.appendFileSync("data/items/items.txt", JSON.stringify(item) + "\n");
+
+  res.status(201).json({
+    id: id,
+    item: itemName
+  });
+})
